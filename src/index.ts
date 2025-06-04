cfffdfc428e63b9ac2f6257b66ea36365e54bf0c
theChosenDevop <tobbey.adesanya@gmail.com>
2025-06-04 16:51:49 +0100

    task endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,39 +10,46 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+export interface JSONRPCError {
+  code: number;
+  message: string;
+  data?: any;
+}
+
 app.get('/.well-known/agent.json', (req: Request, res: Response) => {
   res.send(agentCard);
 });
 
 
-app.post('/task', (req: Request, res: Response) => {
-  const { id, jsonrpc, result, method } = req.body;
-  if (!id || !jsonrpc || !result || !method) {
-     res.status(400).json({ error: 'Invalid request format' });
+app.post('/', (req: Request, res: Response) => {
+  const { id, jsonrpc, params, method } = req.body;
+
+  if (!id || !jsonrpc || !params || !method) {
+    res.status(400).json({
+      jsonrpc: '2.0',
+      id: id || null,
+      error: {
+        code: -32600,
+        message: 'Invalid request format'
+      }});
      return;
   }
-  const {parts} = result;
-  const text  = parts[0].text;
-  const response = {
-    jsonrpc: '2.0',
-    id: id,
-    result: {
-      taskId: result.taskId || null,
-      messageId: result.messageId,
-      contextId: result.contextId,
-      role: 'agent',
-      kind: result.kind,
-      "parts": [
-        {
-          type: 'text',
-          text: `Received your message: ${text}`,
-          metadata: null
-        }
-      ],
-      "metadata": result.metadata || null
-    },
-    method: "message/send"
+  
+const response = {
+  jsonrpc: '2.0',
+  id: id,
+  result: {
+    messageId: '01970dba-d671-770a-a5ae-6eee73e18699',
+    kind: "message",
+    role: 'agent',
+    parts: [
+      {
+        kind: 'text',
+        text: ' I am an agent and I can help you with various tasks.'
+      }
+    ],
   }
+}
   res.status(200).json(response);
   console.log('Received task:', response);
   return;
