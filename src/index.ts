61d03951b156e04b129141c139e4dff779c7f0e7
theChosenDevop <tobbey.adesanya@gmail.com>
2025-06-04 09:55:07 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+import agentCard from './agent.json';
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+app.use(express.json());
+
+app.get('/.well-known/agent.json', (req: Request, res: Response) => {
+  res.send(agentCard);
+});
+
+
+app.post('/task', (req: Request, res: Response) => {
+ console.log('Received task:', req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Agent is running at http://localhost:${port}`);
+});
